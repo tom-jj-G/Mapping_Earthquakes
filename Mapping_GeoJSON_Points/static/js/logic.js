46cc30ef0b309d224d5c73897a2b4df838264c6c
Tom Greff <tomjj.greff@gmail.com>
2020-08-11 20:25:14 -0400

    upload files

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,21 +1,64 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([39.6213, -122.3790], 5);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+    "geometry":{
+        "type":"Point",
+        "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// // Option 1: 
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport).addTo(map);
+
+// Option 2
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
+    }
+  }).addTo(map);
+
+// // Option 3
+// L.geoJson(sanFranAirport, {
+//   onEachFeature: function(feature, layer) {
+//     console.log(layer);
+//     layer.bindPopup("<h2>" + layer.feature.properties.city + "</h2>");
+//    }
+// }).addTo(map);
+
 
 // Coordinates for each point to be used in the line.
-let line = [
-	[33.9416, -118.4085],
-	[37.6213, -122.3790],
-	[40.7899, -111.9791],
-	[47.4502, -122.3088]
-];
+// let line = [
+// 	[33.9416, -118.4085],
+// 	[37.6213, -122.3790],
+// 	[40.7899, -111.9791],
+// 	[47.4502, -122.3088]
+// ];
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-	color: "Yellow"
-}).addTo(map);
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+// 	color: "Yellow"
+// }).addTo(map);
 
 // // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -57,7 +100,7 @@ L.polyline(line, {
 // });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
