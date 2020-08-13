5d98aff1d2c79dea46c87a40419478523b4da09d
Tom Greff <tomjj.greff@gmail.com>
2020-08-13 15:47:01 -0400

    upload files

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,8 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // Add GeoJSON data.
 let sanFranAirport =
@@ -105,5 +105,9 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 	maxZoom: 18,
 	accessToken: API_KEY
 });
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/master/majorAirports.json";
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
