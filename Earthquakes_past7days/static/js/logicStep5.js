83dcc3ace534be00df5e27df1da40b7de2cfffb3
Tom Greff <tomjj.greff@gmail.com>
2020-08-14 10:15:55 -0400

    upload files

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -203,24 +203,34 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   //Then we add the earthquake layer to our map
   earthquakes.addTo(map)
 
-  // Custom legend
-  var legend = L.control({position: 'bottomright'});
+  // Create a legend control object.
+  var legend = L.control({
+	position: 'bottomright'
+  });
 
-  legend.onAdd = function (map) {
-  
-	  var div = L.DomUtil.create('div', 'info legend'),
-		  grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-		  labels = [];
-  
-	  // loop through our density intervals and generate a label with a colored square for each interval
-	  for (var i = 0; i < grades.length; i++) {
-		  div.innerHTML +=
-			  '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-			  grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-	  }
-  
-	  return div;
+  // Then add all the details for the legend.
+  legend.onAdd = function () {
+	let div = L.DomUtil.create('div', 'info legend');
+	const magnitudes = [0, 1, 2, 3, 4, 5];
+	const colors = [
+	"#98ee00",
+	"#d4ee00",
+	"#eecc00",
+	"#ee9c00",
+	"#ea822c",
+	"#ea2c2c"
+	];
+
+	// loop through our intervals to generate a label with a colored square for each interval
+	for (var i = 0; i < magnitudes.length; i++) {
+		console.log(colors[i]);
+		div.innerHTML +=
+			'<i style="background:' + colors[i] + '"></i> ' +
+			magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+	}
+
+	return div;
   };
-  
-  legend.addTo(map);
+
+legend.addTo(map);
 });
